83f2f25f4213a7715b1eedb4bdb28922fe63256e
Philippe Marschall <philippe.marschall@gmail.com>
2016-04-16 22:12:51 +0200

    bump

---
+++ src/main/java/com/github/marschall/pgday2016/MoonLanding.java
@@ -0,0 +1,27 @@
+package com.github.marschall.pgday2016;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.ZoneOffset.UTC;
+
+import java.time.ZoneId;
+
+public class MoonLanding {
+
+  public static void main(String[] args) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss VV");
+
+    ZonedDateTime landingUtc = ZonedDateTime.of(1969, 7, 20, 20, 18, 4, 0, UTC);
+
+    System.out.println(landingUtc.format(formatter));
+    for (String zoneId : new String[]{"Europe/Zurich", "Europe/Moscow", "America/New_York", "Australia/Sydney"}) {
+      System.out.println(landingUtc.withZoneSameInstant(ZoneId.of(zoneId)).format(formatter));
+    }
+
+//    for (String zoneId : ZoneId.getAvailableZoneIds()) {
+//      System.out.println(zoneId);
+//    }
+  }
+
+}
