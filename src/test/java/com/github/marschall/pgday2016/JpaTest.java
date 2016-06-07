605fe79e30a9051f88696594f9a8acc22a4fcdd2
Philippe Marschall <philippe.marschall@gmail.com>
2016-06-07 10:11:08 +0200

    add criteria API

--- src/test/java/com/github/marschall/pgday2016/JpaTest.java
+++ src/test/java/com/github/marschall/pgday2016/JpaTest.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.assertNotNull;
 
 import java.math.BigInteger;
 import java.time.LocalTime;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
@@ -25,11 +29,28 @@ public class JpaTest extends AbstractPosgresTest {
 
   @Test
   public void query() {
+    LocalTime time = LocalTime.of(12, 5);
+
     DemoTable demoTable = (DemoTable) em.createQuery(
             "SELECT d from DemoTable d WHERE d.timeColumn < :time")
-            .setParameter("time", LocalTime.of(12, 5))
+            .setParameter("time", time)
             .getSingleResult();
     assertNotNull(demoTable);
+  }
+
+
+
+  @Test
+  public void criteria() {
+    LocalTime time = LocalTime.of(12, 5);
+
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<DemoTable> cq = cb.createQuery(DemoTable.class);
+    Root<DemoTable> demoTable = cq.from(DemoTable.class);
+    CriteriaQuery<DemoTable> beforeTwelfeFixe = cq.where(
+            cb.lessThan(demoTable.get(DemoTable_.timeColumn), time));
+    List<DemoTable> resultList = em.createQuery(beforeTwelfeFixe).getResultList();
+    assertNotNull(resultList);
   }
 
 }
